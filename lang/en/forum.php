mihailges/moodle

MDL-71915 mod_forum: Update the tertiary nav
Update the tertiary navigation for this
activity.

mod/forum/lang/en/forum.php
@@ -28,6 +28,7 @@ $string['activityoverview'] = 'There are new forum posts';
 $string['actionsforgraderinterface'] = 'Actions for the grader interface';
 $string['actionsforpost'] = 'Actions for post';
 $string['addanewdiscussion'] = 'Add a new discussion topic';
+$string['addnewdiscussion'] = 'Add discussion topic';
 $string['addanewquestion'] = 'Add a new question';
 $string['addanewtopic'] = 'Add a new topic';
 $string['addtofavourites'] = 'Star this discussion';
@@ -675,6 +676,7 @@ $string['subscriberstowithcount'] = 'Subscribers to "{$a->name}" ({$a->count})';
 $string['subscribestart'] = 'Send me notifications of new posts in this forum';
 $string['subscribestop'] = 'I don\'t want to be notified of new posts in this forum';
 $string['subscription'] = 'Subscription';
+$string['subscribetoforum'] = 'Subscribe to forum';
 $string['subscription_help'] = 'If you are subscribed to a forum it means you will receive notification of new forum posts. Usually you can choose whether you wish to be subscribed, though sometimes subscription is forced so that everyone receives notifications.';
 $string['subscriptionandtracking'] = 'Subscription and tracking';
 $string['subscriptionmode'] = 'Subscription mode';
@@ -733,6 +735,7 @@ $string['unsubscribeallconfirmdiscussions'] = 'You are currently subscribed to {
 $string['unsubscribealldone'] = 'All optional forum subscriptions were removed. You will still receive notifications from forums with forced subscription. To manage forum notifications go to Messaging in My Profile Settings.';
 $string['unsubscribeallempty'] = 'You are not subscribed to any forums. To disable all notifications from this server go to Messaging in My Profile Settings.';
 $string['unsubscribed'] = 'Unsubscribed';
+$string['unsubscribefromforum'] = 'Unsubscribe from forum';
 $string['unsubscribeshort'] = 'Unsubscribe';
 $string['useexperimentalui'] = 'Use experimental nested discussion view';
 $string['usermarksread'] = 'Manual message read marking';

mod/forum/renderer.php
@@ -234,10 +234,32 @@ class mod_forum_renderer extends plugin_renderer_base {
      * Render quick search form.
      *
      * @param \mod_forum\output\quick_search_form $form The renderable.
-     * @return string
+     * @return string rendered HTML string from the template.
      */
     public function render_quick_search_form(\mod_forum\output\quick_search_form $form) {
-        return $this->render_from_template('mod_forum/quick_search_form', $form->export_for_template($this));
+        return $this->render_from_template('mod_forum/forum_new_discussion_actionbar', $form->export_for_template($this));
+    }
+
+    /**
+     * Render the view action area.
+     *
+     * @param \mod_forum\output\activity_actionbar $actionbar activity_actionbar object.
+     * @return string rendered HTML string
+     */
+    public function render_activity_actionbar(\mod_forum\output\activity_actionbar $actionbar):string {
+        return $this->render_from_template('mod_forum/forum_action_bar', $actionbar->export_for_template($this));
+    }
+
+    /**
+     * Render the subscription action area.
+     *
+     * @param \mod_forum\output\subscription_actionbar $subscriptionactionbar subscription_actionbar object.
+     * @return bool|string rendered HTML string.
+     */
+    public function subscription_actionbar(\mod_forum\output\subscription_actionbar $subscriptionactionbar):string {
+        return $this->render_from_template(
+                'mod_forum/forum_subscription_action',
+                $subscriptionactionbar->export_for_template($this));
     }
 
     /**

wjlc60/forum-ai

lang/en/forum.php
@@ -27,7 +27,7 @@ $string['activitydate:due'] = 'Due:';
 $string['activityoverview'] = 'There are new forum posts';
 $string['actionsforgraderinterface'] = 'Actions for the grader interface';
 $string['actionsforpost'] = 'Actions for post';
-$string['addanewdiscussion'] = 'Add a new discussion topic';
+$string['addanewdiscussion'] = 'Add discussion topic';
 $string['addanewquestion'] = 'Add a new question';
 $string['addanewtopic'] = 'Add a new topic';
 $string['addtofavourites'] = 'Star this discussion';
@@ -664,7 +664,7 @@ $string['smallmessage'] = '{$a->user} posted in {$a->forumname}';
 $string['smallmessagedigest'] = 'Forum digest containing {$a} messages';
 $string['startedby'] = 'Started by';
 $string['subject'] = 'Subject';
-$string['subscribe'] = 'Subscribe to this forum';
+$string['subscribe'] = 'Subscribe to forum';
 $string['subscribediscussion'] = 'Subscribe to this discussion';
 $string['subscribeall'] = 'Subscribe everyone to this forum';
 $string['subscribeenrolledonly'] = 'Sorry, only enrolled users are allowed to subscribe to forum post notifications.';
@@ -722,7 +722,7 @@ $string['unreadpost'] = 'Unread post';
 $string['unreadposts'] = 'Unread posts';
 $string['unreadpostsnumber'] = '{$a} unread posts';
 $string['unreadpostsone'] = '1 unread post';
-$string['unsubscribe'] = 'Unsubscribe from this forum';
+$string['unsubscribe'] = 'Unsubscribe from forum';
 $string['unsubscribelink'] = 'Unsubscribe from this forum: {$a}';
 $string['unsubscribediscussion'] = 'Unsubscribe from this discussion';
 $string['unsubscribediscussionlink'] = 'Unsubscribe from this discussion: {$a}';

renderer.php
@@ -234,10 +234,35 @@ class mod_forum_renderer extends plugin_renderer_base {
      * Render quick search form.
      *
      * @param \mod_forum\output\quick_search_form $form The renderable.
-     * @return string
+     * @return string rendered HTML string from the template.
      */
     public function render_quick_search_form(\mod_forum\output\quick_search_form $form) {
-        return $this->render_from_template('mod_forum/quick_search_form', $form->export_for_template($this));
+        if (strpos($this->page->url->get_path(), "index.php")) {
+            return $this->render_from_template('mod_forum/quick_search_form', $form->export_for_template($this));
+        }
+
+        return $this->render_from_template('mod_forum/forum_new_discussion_actionbar', $form->export_for_template($this));
+    }
+
+    /**
+     * Render the view action area.
+     *
+     * @param \mod_forum\output\forum_actionbar $actionbar forum_actionbar object.
+     * @return string rendered HTML string
+     */
+    public function render_activity_actionbar(\mod_forum\output\forum_actionbar $actionbar): string {
+        return $this->render_from_template('mod_forum/forum_actionbar', $actionbar->export_for_template($this));
+    }
+
+    /**
+     * Render the subscription action area.
+     *
+     * @param \mod_forum\output\subscription_actionbar $subscriptionactionbar subscription_actionbar object.
+     * @return bool|string rendered HTML string.
+     */
+    public function subscription_actionbar(\mod_forum\output\subscription_actionbar $subscriptionactionbar): string {
+        return $this->render_from_template('mod_forum/forum_subscription_action',
+            $subscriptionactionbar->export_for_template($this));
     }
 
     /**