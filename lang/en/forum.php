nisaDoc/moodle

MDL-71744 forum: added announcement forum defaults

mod/forum/lang/en/forum.php
@@ -41,6 +41,8 @@ $string['allsubscribe'] = 'Subscribe to all forums';
 $string['allunsubscribe'] = 'Unsubscribe from all forums';
 $string['allusers'] = 'All users';
 $string['alreadyfirstpost'] = 'This is already the first post in the discussion';
+$string['announcementsettings'] = 'Default Announcement Forum Settings';
+$string['announcementsettings_help'] = 'These settings define the defaults for all announcement forum.';
 $string['anyfile'] = 'Any file';
 $string['areaattachment'] = 'Attachments';
 $string['areapost'] = 'Messages';

mod/forum/lib.php
@@ -2092,7 +2092,8 @@ function forum_get_course_forum($courseid, $type) {
             $forum->name  = get_string("namenews", "forum");
             $forum->intro = get_string("intronews", "forum");
             $forum->introformat = FORMAT_HTML;
-            $forum->forcesubscribe = FORUM_FORCESUBSCRIBE;
+            $forum->forcesubscribe = $CFG->forum_announcement_subscription;
+            $forum->maxattachments = $CFG->forum_announcement_maxattachments;
             $forum->assessed = 0;
             if ($courseid == SITEID) {
                 $forum->name  = get_string("sitenews");

mod/forum/settings.php
@@ -138,5 +138,17 @@ if ($ADMIN->fulltree) {
 
     $settings->add(new admin_setting_configcheckbox('forum_enabletimedposts', get_string('timedposts', 'forum'),
                        get_string('configenabletimedposts', 'forum'), 1));
+
+    $settings->add(new admin_setting_heading('defaultsettings', get_string('announcementsettings', 'mod_forum'),
+                        get_string('announcementsettings_help', 'mod_forum')));
+
+    // Default number of attachments allowed per post in announcement forums.
+    $settings->add(new admin_setting_configtext('forum_announcement_maxattachments', get_string('maxattachments', 'forum'),
+                       get_string('configmaxattachments', 'forum'), 1, PARAM_INT));
+
+    // Default Subscription mode setting for announcement forums.
+    $options = forum_get_subscriptionmode_options();
+    $settings->add(new admin_setting_configselect('forum_announcement_subscription', get_string('subscriptionmode', 'forum'),
+        get_string('configsubscriptiontype', 'forum'), FORUM_FORCESUBSCRIBE, $options));
 }
 

wjlc60/forum-ai

lang/en/forum.php
@@ -42,6 +42,8 @@ $string['allsubscribe'] = 'Subscribe to all forums';
 $string['allunsubscribe'] = 'Unsubscribe from all forums';
 $string['allusers'] = 'All users';
 $string['alreadyfirstpost'] = 'This is already the first post in the discussion';
+$string['announcementsettings'] = 'Default Announcement Forum Settings';
+$string['announcementsettings_help'] = 'These settings define the defaults for all announcement forums.';
 $string['anyfile'] = 'Any file';
 $string['areaattachment'] = 'Attachments';
 $string['areapost'] = 'Messages';

lib.php
@@ -2100,7 +2100,8 @@ function forum_get_course_forum($courseid, $type) {
             $forum->name  = get_string("namenews", "forum");
             $forum->intro = get_string("intronews", "forum");
             $forum->introformat = FORMAT_HTML;
-            $forum->forcesubscribe = FORUM_FORCESUBSCRIBE;
+            $forum->forcesubscribe = $CFG->forum_announcementsubscription;
+            $forum->maxattachments = $CFG->forum_announcementmaxattachments;
             $forum->assessed = 0;
             if ($courseid == SITEID) {
                 $forum->name  = get_string("sitenews");

settings.php
@@ -138,5 +138,17 @@ if ($ADMIN->fulltree) {
 
     $settings->add(new admin_setting_configcheckbox('forum_enabletimedposts', get_string('timedposts', 'forum'),
                        get_string('configenabletimedposts', 'forum'), 1));
+
+    $settings->add(new admin_setting_heading('defaultsettings', get_string('announcementsettings', 'mod_forum'),
+                        get_string('announcementsettings_help', 'mod_forum')));
+
+    // Default number of attachments allowed per post in announcement forums.
+    $settings->add(new admin_setting_configtext('forum_announcementmaxattachments', get_string('maxattachments', 'forum'),
+                       get_string('configmaxattachments', 'forum'), 1, PARAM_INT));
+
+    // Default Subscription mode setting for announcement forums.
+    $options = forum_get_subscriptionmode_options();
+    $settings->add(new admin_setting_configselect('forum_announcementsubscription', get_string('subscriptionmode', 'forum'),
+        get_string('configsubscriptiontype', 'forum'), FORUM_FORCESUBSCRIBE, $options));
 }
 